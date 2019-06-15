50258461ec880b270a47f1b48c6883af4b2510f6
gstiedem <oleg.plakida@gmail.com>
2019-06-15 15:50:18 +0300

    0.11

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(256), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    photo = db.Column(db.String(256), unique=True, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -22,83 +24,40 @@ class User(db.Model):
 
 @app.route("/")
 def index():
-    """ INDEX PAGE """
-    # User id
-    user_id = session["user_id"]
-
-
     # Render the final template
     return render_template("index.html")
 
 
-@app.route("/client", methods=["GET", "POST"])
-def client_page():
-
-    if request.method == "GET":
-        return render_template("client.html")
-
-@app.route("/corp")
+@app.route("/company")
 def corp_page():
-    """Show history of transactions"""
-
-    user_id = session["user_id"]
-
-    return render_template("history.html", history = history)
+    """Get user info"""
 
 
-@app.route("/login", methods=["GET", "POST"])
+    return render_template("company.html")
+
+
+@app.route("/client", methods=["GET", "POST"])
 def login():
     """Log user in"""
 
-    # Forget any user_id
-    session.clear()
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
+        user = User.query.filter_by(username=request.form.get("username")).first()
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        if not user.username and not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template("404.html")
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = user.id
 
         # Redirect user to home page
         return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("login.html")
+        return render_template("index.html")
 
-
-@app.route("/logout")
-def logout():
-    """Log user out"""
-
-    # Forget any user_id
-    session.clear()
-
-    # Redirect user to login form
-    return redirect("/")
-
-
-def errorhandler(e):
-    """Handle error"""
-    return apology(e.name, e.code)
-
-
-# listen for errors
-for code in default_exceptions:
-    app.errorhandler(code)(errorhandler)
